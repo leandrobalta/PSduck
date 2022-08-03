dfac0e77ebeaad1d97da082257dccd92d681769f
Leandro Braga <leandro@ezaccess.com.br>
2022-08-03 18:00:19 -0300

    update of day 03/08/22

--- Program.py
+++ Program.py
@@ -1,26 +1,55 @@
 from psutil import (
     cpu_percent,
     cpu_freq,
+    disk_partitions,
+    disk_usage,
     virtual_memory,
-    swap_memory
+    swap_memory,
+    sensors_temperatures,
+    sensors_battery,
+    disk_usage
 )
-from dashing import HSplit, VSplit, VGauge, HGauge
+from dashing import HSplit, VSplit, VGauge, HGauge, Text
 from time import sleep
 
 user_interface = HSplit(  # user_interface have items
     HSplit(  # RAM -->   user_interface.items[0]
         # user_interface.items[0].items[0] -> this is to access this scope of the "user_interface"
+
         VGauge(title="RAM"),
         VGauge(title='SWAP'),
         title="MEMORY",
         border_color=2
     ),
-    HSplit(
-        HGauge(title='CPU'),
+    VSplit(  # CPU --> user_interface.items[1]
+
+        HGauge(title='CPU (MAIN)'),
+        HGauge(title='CPU_1'),
+        HGauge(title='CPU_2'),
+        HGauge(title='CPU_3'),
+        HGauge(title='CPU_4'),
+        HGauge(title='CPU TEMP'),
         title="CPU MONITOR",
         border_color=5
+    ),
+    VSplit(  # DISK AND OTHERS --> user_interface.items[2]
+        Text(  # DISK --> user_interface.items[2].items[0]
+            ' ',
+            title="DISK INFO",
+            border_color=1,
+        ),
+        VSplit(  # OTHERS --> user_interface.items[2].items[1]
+            HGauge(title="BATTERY"),
+            title="OTHERS",
+            border_color=3
+        ),
     )
 )
+
+
+def ConvertToGiga(value):
+    return value / 1024 / 1024 / 1024
+
 
 while True:
     # MEMORY
@@ -44,6 +73,37 @@ while True:
     principal_cpu = cpu_ui.items[0]
     principal_cpu.value = cpu_percent()
     principal_cpu.title = f" CPU {principal_cpu.value}%"
+
+    # the other cpu cores
+    cpu_cores_ui = cpu_ui.items[1:5]
+    all_cores_percent = cpu_percent(percpu=True)
+
+    # cpu temperature
+    cpu_temp_ui = cpu_ui.items[5]
+    temp = sensors_temperatures()['coretemp'][0]
+    cpu_temp_ui.value = temp.current
+    cpu_temp_ui.title = f" CPU TEMP {cpu_temp_ui.value}C"
+
+    # --------------------------------------------------
+
+    # DISK
+    disk_ui = user_interface.items[2].items[0]
+    disk_used = disk_usage('/')
+    used = ConvertToGiga(disk_used.used)
+    disk_ui.value = used
+    disk_ui.text = f"DISK USED {disk_ui.value:.2f}GB"
+
+    # --------------------------------------------------
+
+    # BATTERY
+    battery_ui = user_interface.items[2].items[1]
+    battery = sensors_battery()
+    battery_ui.value = battery.percent
+    battery_ui.title = f" BATTERY {battery_ui.value}%"
+
+    for i, (core, value) in enumerate(zip(cpu_cores_ui, all_cores_percent)):
+        core.value = value
+        core.title = f" CPU_{i + 1} {value}%"
 
     try:
         user_interface.display()
--- Test_to_Query.py
+++ Test_to_Query.py
@@ -1,4 +1,52 @@
-from psutil import swap_memory
+from re import T
+from psutil import sensors_battery, swap_memory, cpu_count, cpu_stats, cpu_percent, cpu_freq, disk_io_counters, disk_partitions, disk_usage, sensors_temperatures
 
 
-print(swap_memory())
+# print(swap_memory())
+# print(cpu_count())
+# print(cpu_stats())
+print(cpu_freq(cpu_percent(percpu=True)))
+
+# print(cpu_percent(pe))
+
+
+print()
+print()
+print()
+
+print(cpu_percent())
+
+
+print()
+print()
+print()
+
+print(disk_usage('/'))
+
+print()
+print()
+print()
+
+for i in enumerate(disk_usage('/')):
+    print(i)
+
+print()
+print()
+print()
+
+
+for i in sensors_temperatures():
+    print(i)
+
+print()
+
+test = (sensors_temperatures()['coretemp'][0])
+
+print(f" {test.current}C")
+
+print()
+print()
+print()
+
+battery_percent = sensors_battery()
+print(battery_percent.percent)
